30330bd875df1e4e132520f3973b845512cf233b
Pavan Pucha <pavanrajasekhar@gmail.com>
2023-11-01 23:46:25 -0400

    init proj

---
+++ bin/map_reduce.dart
@@ -0,0 +1,5 @@
+import 'package:map_reduce/map_reduce.dart' as map_reduce;
+
+void main(List<String> arguments) {
+  print('Hello world: ${map_reduce.calculate()}!');
+}
---
+++ test/map_reduce_test.dart
@@ -0,0 +1,8 @@
+import 'package:map_reduce/map_reduce.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
